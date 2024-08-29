35504f0a4609dc5dc8f335c46af22df6b8fccca6
oki <t.cpp@outlook.com>
2024-08-30 01:16:04 +0800

    add port

--- src/headless.rs
+++ src/headless.rs
@@ -8,6 +8,7 @@ pub fn browse_page() -> Result<Vec<u8>, Box<dyn Error>> {
         .headless(true)
         .devtools(false)
         .sandbox(false)
+        .port(Some(8010))
         // 这里宽高是 window 的，不是 view 的，注意概念区别
         .window_size(Some((1600, 1200)))
         .build()
