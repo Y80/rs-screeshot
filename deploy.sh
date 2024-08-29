8b30d1668a61fa43ef4c5623a544769644f52425
oki <t.cpp@outlook.com>
2024-08-30 01:10:40 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,13 @@ exec_path="$HOME/codes/rs-screenshot/target/release/screenshot"
 
 # 如果进程正在运行，则杀死它
 if pgrep -f "$exec_path" > /dev/null; then
+  echo "杀死进程: $(pgrep -f "$exec_path")"
   kill $(pgrep -f "$exec_path")
 fi
 
 # 重新启动进程，并将输出重定向到指定文件
 nohup "$exec_path" > "$HOME/codes/rs-screenshot/screenshot.log" 2>&1 &
+
+echo "后台任务已创建"
+
+ps aux | grep $exec_path
